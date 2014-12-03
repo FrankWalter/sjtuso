236ff9a8f85f2c33110ae05c3185f0945a530316
Ahriy <liuzhe314@qq.com>
2014-12-03 14:48:08 +0800

    liuzhe

--- application/views/header.php
+++ application/views/header.php
@@ -3,7 +3,56 @@
 <head>
   <meta charset="utf-8">
   <title>交大搜索</title>
-  <script type="text/javascript" src="http://cdn.amazeui.org/amazeui/1.0.1/css/amazeui.basic.min.css"></script>
-  <script type="text/javascript" src="http://cdn.amazeui.org/amazeui/1.0.1/js/amazeui.min.js"></script>
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/amazeui.min.css">
+  <link rel="stylesheet" href="php echo base_url(); ?>assets/css/admin.css">
+  <script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/amazeui.min.js"></script>
+<!--<![endif]-->
+<script src="assets/js/app.js"></script>
+
+
+    <style>
+    .header {
+      text-align: center;
+    }
+    .header h1 {
+      font-size: 200%;
+      color: #333;
+      margin-top: 30px;
+    }
+    .header p {
+      font-size: 14px;
+    }
+    .am-g .am-form-group{
+
+      padding-left: 0px;
+      padding-right: 0px;
+    }
+
+
+
+    /*style for navbar*/
+    .am-menu {
+      margin-left: 300px;
+    }
+    /*style for search form*/
+    button {
+      height: 39px;
+      margin-left: 70px;
+    }
+
+   input{
+      margin-left: 300px;
+    }
+    .am-form-field {
+      width: 75%;
+    }
+
+    .am-accordion-title{
+      margin-left: 300px;
+    }
+    .am-accordion-content{
+      margin-left: 300px;
+    }
+  </style>
 </head>
-<body>
--- application/views/search.php
+++ application/views/search.php
@@ -1,18 +1,90 @@
+<!-- the header of the web -->
 <body>
+  <div class="header">
+    <div class="am-g">
+      <h1>SJTU.so</h1>
+      <img src="<?php echo base_url()?>/src/img/header.png">
+    </div>
+    <hr />
+  </div>
 
-<div id="container">
-  <h1>交大搜索</h1>
+  <!-- the nvabar -->
+  <div class="am-g">
+    <nav data-am-widget="menu" class="am-menu  am-menu-default">
 
-  <form class="am-form" method="GET" action="welcome/search">
+      <ul class="am-menu-nav am-avg-sm-3">
+        <li class="">
+          <a href="##">新闻</a>
+        </li>
+        <li class="">
+          <a href="##">教师</a>
+        </li>
+        <li class="">
+          <a href="##">实验室</a>
+        </li>
+      </li>
+      <li class="">
+        <a href="##">论文</a>
+      </li>
+      <li class="">
+        <a href="##">图书</a>
+      </li>
+      <li class="">
+        <a href="##">课程</a>
+      </li>
+    </li>
+    <li class="">
+      <a href="##">学生活动</a>
+    </li>
+  </li>
+  <li class="">
+    <a href="##">竞赛</a>
+  </li>
+</ul>
+</nav>
+</div>
 
-    <div class="am-form-group">
-      <input type="text" class="" name="q" placeholder="关键字">
+
+<div class="am-g">
+  <!-- the main search form -->
+  <form role="search">
+    <div class="am-u-sm-8 am-form-group">
+      <input type="text" class="am-form-field am-input-sm" placeholder="Ready to Search">
+
     </div>
 
-    <p><button type="submit" class="am-btn am-btn-default">提交</button></p>
+    <div class="am-u-sm-4 am-form-group">
+      <button type="submit" class="am-btn am-btn-default am-btn-sm">Search</button>
+    </div>
   </form>
-  
+
+</br>
+</br>
+</br>
+</br>
+</br>
+<!-- 友情链接 -->
+<section data-am-widget="accordion" class="am-accordion am-accordion-basic"
+data-am-accordion='{  }'>
+<dl class="am-accordion-item am-active">
+  <dt class="am-accordion-title">友情链接</dt>
+  <dd class="am-accordion-bd am-collapse am-in">
+    <!-- 规避 Collapase 处理有 padding 的折叠内容计算计算有误问题， 加一个容器 -->
+    <div class="am-accordion-content">
+      <ul style="list-style-type:none">
+        <li><a href="http://bookex.org/">BookEx</a></li>
+        <li><a href="http://www.tongqu.me/">同去网</a></li>
+        <li><a href="https://bbs.sjtu.edu.cn/file/bbs/index/index.htm">饮水思源BBS</a></li>
+        <li><a href="http://tieba.baidu.com/f?kw=%C9%CF%BA%A3%BD%BB%CD%A8%B4%F3%D1%A7">上海交通大学吧</a></li>
+        <li><a href="https://pt.sjtu.edu.cn/index.php">葡萄</a></li>
+      </ul>
+    </dd>
+  </dl>
+</section>
 </div>
+
+
+
 
 </body>
 </html>
