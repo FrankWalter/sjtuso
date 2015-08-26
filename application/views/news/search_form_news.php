b4410cfac60df74f09be6b79ef8658185ff2ec2d
Ahriy <liuzhe314@qq.com>
2015-08-26 14:08:31 +0800

    add UI
    add UI

--- application/views/news/search_form_news.php
+++ application/views/news/search_form_news.php
@@ -2,7 +2,7 @@
       <!-- the main search form -->
       <form role="search" method="GET" action="<?php echo site_url('index.php/search_news'); ?>">
         <div class="am-u-sm-1 am-form-group" id="search_logo">
-          <h1><a href="<?php echo site_url();?>">SJTUso</a></h1>
+          <h1><a href="<?php echo site_url();?>">SJTUso1</a></h1>
         </div>
 
         <div class="am-u-sm-6 am-form-group" id="search_form">
@@ -14,4 +14,41 @@
         </div>
       </form>
     </div>
- 
+ <div class="am-g">
+    <div class="am-u-sm-3"> </div>
+    <div class="am-u-sm-9"> 
+
+      <nav data-am-widget="menu" class="am-menu  am-menu-default">
+
+        <ul class="am-menu-nav am-avg-sm-3">
+          <li class="">
+            <a href="##">新闻</a>
+          </li>
+          <li class="">
+            <a href="##">教师</a>
+          </li>
+          <li class="">
+            <a href="##">实验室</a>
+          </li>
+        </li>
+        <li class="">
+          <a href="##">论文</a>
+        </li>
+        <li class="">
+          <a href="##">图书</a>
+        </li>
+        <li class="">
+          <a href="##">课程</a>
+        </li>
+      </li>
+      <li class="">
+        <a href="##">学生活动</a>
+      </li>
+    </li>
+    <li class="">
+      <a href="##">竞赛</a>
+    </li>
+  </ul>
+</nav>
+</div>
+</div>
--- application/views/news/search_news.php
+++ application/views/news/search_news.php
@@ -17,7 +17,7 @@
 
         <ul class="am-menu-nav am-avg-sm-3">
           <li class="">
-            <a href="##">新闻</a>
+            <a href="##">新闻1</a>
           </li>
           <li class="">
             <a href="##">教师</a>
